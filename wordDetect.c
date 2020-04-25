9743907f263d9d94eab8fc8dd55e75858843280c
pshivam111 <pshivam111@gmail.com>
2020-04-25 12:03:05 +0530

    word detection

--- wordDetect.c
+++ wordDetect.c
@@ -1,9 +1,22 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
 char s[20];
+char p[]=" My name is shivam";
 scanf("%s",s);
 printf("\n %s",s);
-
+printf("%d",strlen(s));
+int state=0;
+for(int i=0;i<strlen(p);i++)
+{   if(state==strlen(s))
+    {
+        printf("Detected");
+    }
+    else
+    {
+        
+    }
+}
 
 }
